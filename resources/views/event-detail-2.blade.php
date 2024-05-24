a8ccf121de015025f945441871e778e9b243292a
IntelliT <c14210275@john.petra.ac.id>
2024-05-24 13:41:11 +0700

    event-detail.php desain baru

--- resources/views/event-detail-2.blade.php
+++ resources/views/event-detail-2.blade.php
@@ -15,14 +15,21 @@
     <style>
         :root {
             --default-font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", Helvetica, Arial, "PingFang SC", "Hiragino Sans GB", "Microsoft Yahei UI", "Microsoft Yahei", "Source Han Sans CN", sans-serif;
+            --primary-color: #eeb120;
+            --secondary-color: #646464;
+            --background-color: #f4f4f4;
+            --text-color: #000000;
+            --border-color: #000000;
+            --button-background: #000000;
+            --button-text-color: #ffffff;
         }
         body, html {
             margin: 0;
             padding: 0;
             font-family: var(--default-font-family);
+            background-color: var(--background-color);
         }
         .main-container {
-            width: 100%;
             max-width: 1440px;
             margin: 0 auto;
             background-color: #f4f4f4;
@@ -30,11 +37,11 @@
         .content {
             width: calc(100% - 100px);
             max-width: 1337px;
-            background-color: #ffffff;
+            background: #fff;
             margin: 95px auto 35px auto;
-            padding: 35px;
-            box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+            padding: 20px;
             border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0,0,0,0.1);
         }
         .back-button {
             display: flex;
@@ -49,7 +56,7 @@
             font-weight: 600;
             margin-bottom: 20px;
         }
-        .event-image {
+        .content .event-image {
             width: 100%;
             height: auto;
             max-width: 1169px;
@@ -57,32 +64,36 @@
             background: url(./assets/images/405b79a68a003c32aab00353cd2b98feedf55647.png) no-repeat center;
             background-size: cover;
         }
-        .event-header {
+        .content .event-header {
             display: flex;
             justify-content: space-between;
             align-items: center;
             margin: 20px 0;
         }
-        .event-title {
+        .content .event-title {
             font-family: Kanit, var(--default-font-family);
             font-size: 48px;
             font-weight: 600;
         }
-        .register-button {
+        .content .event-title h1 {
+            font-size: 2em;
+            margin: 0;
+        } 
+        .content .register-button {
             display: flex;
             align-items: center;
             justify-content: center;
             padding: 16px 24px;
-            background-color: #eeb120;
+            background: var(--primary-color);
             border: none;
             border-radius: 40px;
             font-family: DM Sans, var(--default-font-family);
-            font-size: 18px;
+            font-size: 1em;
             font-weight: 500;
-            color: #ffffff;
+            color: #fff;
             cursor: pointer;
         }
-        .event-info {
+        .content .event-info {
             display: flex;
             align-items: center;
             gap: 20px;
@@ -92,14 +103,124 @@
             font-weight: 500;
             color: #646464;
         }
-        .event-description {
+        .content .event-description {
             font-family: Roboto, var(--default-font-family);
-            font-size: 24px;
+            font-size: 1em;
             font-weight: 400;
-            color: #646464;
+            color: var(--secondary-color);;
             line-height: 1.5;
             letter-spacing: 0.48px;
-            margin-bottom: 40px;
+            margin: 20px 0;
+        }
+        .modal-event-title {
+            font-weight: bold;
+            font-size: 1.5em;
+            color: black;
+            font-family: 'Kanit', sans-serif;
+        }
+        .form-label-daftar {
+            font-family: 'Montserrat', sans-serif;
+            font-size: 1.11em;
+            font-weight: normal;
+        }
+        .form label {
+            font-size: 1em;
+            color: var(--text-color);
+            font-family: 'Montserrat', sans-serif;
+            font-size: 1.33em;
+            font-weight: medium;
+        }
+        .modal-body small {
+            font-family: 'Montserrat', sans-serif;
+            font-size: 1.11em;
+            font-weight: medium;
+            color: #6D6D6D;
+        }
+        .form textarea {
+            padding: 10px;
+            font-size: 1em;
+            border: 1px solid var(--border-color);
+            border-radius: 5px;
+            width: 100%;
+        }
+        .image-container {
+            position: relative;
+            /* Set position to relative */
+        }
+        .image-text-container {
+            position: absolute;
+            /* Set position to absolute */
+            top: 50%;
+            /* Align to the vertical center */
+            left: 50%;
+            /* Align to the horizontal center */
+            transform: translate(-50%, -50%);
+            /* Center the text */
+            text-align: center;
+            /* Center align the text */
+            width: 90%;
+            /* Set the width to 80% of its container */
+        }
+        .image-text-1 {
+            font-family: 'Roboto Slab', serif;
+            /* Use Roboto Slab font */
+            font-weight: bold;
+            font-size: 1.77em;
+            color: white;
+            width: 100%;
+            /* Set the width to 100% */
+            margin: 0 auto;
+            /* Center the element horizontally */
+            text-align: center;
+            /* Center the text horizontally */
+            display: flex;
+            /* Use flexbox */
+            justify-content: center;
+            /* Center the content horizontally */
+            align-items: center;
+            /* Center the content vertically */
+            padding: 20px;
+            /* Increase padding to provide more space around the text */
+            box-sizing: border-box;
+            /* Include padding in the width calculation */
+        }
+        .image-text-2 {
+            font-family: 'Inter', sans-serif;
+            font-size: 0.83em;
+            color: white;
+        }
+        .image-container img {
+            width: 100%;
+            /* Ensure the image fills its container */
+            height: auto;
+            /* Maintain aspect ratio */
+        }
+        .form .modal-footer {
+            display: flex;
+            justify-content: space-between;
+            gap: 20px;
+        }
+        .form-container .modal-footer button {
+            padding: 10px 20px;
+            font-size: 1em;
+            border: none;
+            border-radius: 25px;
+            cursor: pointer;
+        }
+        .form-container .modal-footer .cancel-button {
+            background: #ccc;
+        }
+        .form-container .modal-footer .submit-button {
+            background: var(--button-background);
+            color: var(--button-text-color);
+        }
+
+        /* Set max-width for larger screens */
+        @media (min-width: 1200px) {
+            .modal-dialog {
+                max-width: 982px;
+                /* Set max-width to 982px for screens wider than 1200px */
+            }
         }
     </style>
 </head>
@@ -177,7 +298,7 @@
             <div class="event-image"></div>
             <div class="event-header">
                 <h1 class="event-title">YESS Leardership Mission Training VII</h1>
-                <button class="register-button">
+                <button class="register-button" data-bs-toggle="modal" data-bs-target="#daftarModal">
                     Daftar
                     <span class="material-icons">arrow_forward</span>
                 </button>
@@ -195,7 +316,46 @@
                 Uis Neno nokan kit, Immanuel!<br />
                 Sampe ketemu di YLMT, basodara dong!
             </p>
+
+        <!-- Modal -->
+        <div class="modal fade" id="daftarModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title modal-event-title" id="exampleModalLabel">PENDAFTARAN EVENT</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Your form content goes here -->
+                        <label for="daftar" class="form-label form-label-daftar">Bagi jemaat yang ingin
+                            mendaftar dalam event YESS Surabaya silahkan mengisi form pendaftaran event dibawah ini.</label>
+                        <form>
+                            <div class="mb-3">
+                                <label for="allergies" class="form-label">Alergi Anda (jika tidak punya, isi "-")</label>
+                                <textarea class="form-control" id="allergies"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="familyInfo" class="form-label">Informasi keluarga yang dapat dihubungi</label>
+                                <textarea class="form-control" id="familyInfo"></textarea>
+                                <small class="form-text text-muted">*Sertakan nama dan nomor telepon aktif</small>
+                            </div>
+                        </form>
+                        <div class="image-container">
+                            <div class="image-text-container">
+                                <p class="image-text-1">Cari circle rohani yang sehat dan bikin semangat?</p>
+                                <p class="image-text-2">YUK SINI MERAPAT!</p>
+                            </div>
+                            <img src="img/form.jpg" alt="Image">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
+                        <button type="button" class="btn btn-primary">SUBMIT</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
         <!-- Footer -->
         <div class="container-fluid" style="background-color: black; color: white; border-radius: 30px 30px 0 0;">
             <div class="row">
