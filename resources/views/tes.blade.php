b186b658aef4647d30e42d86a11a97aa5205b348
IntelliT <c14210275@john.petra.ac.id>
2024-06-24 20:12:28 +0700

    event berbayar frontend
    Bisa trigger modalnya, ya

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -6,37 +6,48 @@
   <title>Bootstrap Row and Col Example</title>
   <!-- Link to Bootstrap CSS -->
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <style>
-    .col-proportional {
-      flex: 1 0 auto;
-      word-wrap: break-word;
-      white-space: normal;
-    }
+    .modal-body > .img-responsive {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 80%;
+    height: auto;
+}
   </style>
 </head>
 <body>
 
-<div class="container mt-4">
-  <!-- Elemen di atas dengan width: 100% -->
-  <div class="row">
-    <div class="col-12" style="background-color: lightblue;">
-      Elemen dengan width 100%
-    </div>
-  </div>
-  <!-- Row dengan dua kolom -->
-  <div class="row" style="background-color: lightgray;">
-    <div class="col d-flex flex-wrap p-2" style="width: 100%;">
-      <div class="col-proportional bg-primary text-white m-2 p-2">
-        Kolom 1 dengan teks yang cukup panjang agar bisa turun ke baris berikutnya jika diperlukan.
-      </div>
-      <div class="col-proportional bg-secondary text-white m-2 p-2">
-        Kolom 2 dengan teks yang lebih panjang lagi agar bisa turun ke baris berikutnya jika diperlukan. Ini untuk memastikan bahwa pengaturan flexbox dan CSS kita bekerja dengan baik.
-      </div>
-    </div>
-  </div>
-</div>
+        <!-- Modal bayar -->
+        <div class="modal fade modal-bayar" id="bayarModal" tabindex="-1"
+            aria-labelledby="bayarModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered modal-custom-width">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title modal-event-title" id="bayarModalLabel">
+                            BAYAR
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Your form content goes here -->
+                        <img class="img-responsive" src="img/qris.jpg" alt="QRIS">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bayarModal">
+  Bayar
+</button>
 
 <!-- Link to Bootstrap JS and dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
